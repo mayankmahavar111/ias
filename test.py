711580521c9b06bcc199a7284f5e4fc177ca5a19
Manu9948 <manoharmadanu@gmail.com>
2018-02-14 22:43:50 +0530

    manu 3

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-import whois
-url = 'google.com'
-w = whois.whois(url)
-print w
+#!/usr/bin/env python
+import urllib, sys, re
+temp='stackoverflow.com'
+xml = urllib.urlopen('http://data.alexa.com/data?cli=10&dat=s&url=%s'%temp).read()
+try: rank = int(re.search(r'<POPULARITY[^>]*TEXT="(\d+)"', xml).groups()[0])
+except Exception as e:
+    print e
+    rank = -1
+print 'Your rank for %s is %d!\n' % (temp, rank)
