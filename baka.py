64847ca1279a41fd80fab82a53cea87a7bf3ab17
Manu9948 <manoharmadanu@gmail.com>
2018-02-15 22:45:40 +0530

    manu4

--- baka.py
+++ baka.py
@@ -1,10 +1,52 @@
-#!/usr/bin/env python
-"""import urllib, sys, re
-temp='stackoverflow.com'
-xml = urllib.urlopen('http://data.alexa.com/data?cli=10&dat=s&url=%s'%temp).read()
-try: rank = int(re.search(r'<POPULARITY[^>]*TEXT="(\d+)"', xml).groups()[0])
-except Exception as e:
-    print e
-    rank = -1
-print 'Your rank for %s is %d!\n' % (temp, rank)
-"""
+import requests
+import time
+from bs4 import BeautifulSoup
+from urllib import urlencode
+from urllib import urlopen
+
+# Get sitemap
+
+sitemap_url = 'https://pythonmatplotlibtips.blogspot.com/sitemap.xml'
+data = urlopen(sitemap_url).read()
+data = str(data)
+splited = data.replace("</loc>","<loc>").split("<loc>")
+urls = [ url for url in splited if 'pythonmatplotlibtips.blogspot.com' in url]
+
+# Check if the url has been indexed
+
+seconds = 10
+
+proxies = {
+    'https' : 'https://localhost:8123',
+    'http' : 'http://localhost:8123'
+    }
+
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+headers = { 'User-Agent' : user_agent}
+
+indexed = []
+notindexed = []
+retvals = []
+for line in urls:
+    query = {'q': 'info:' + line}
+    google = "https://www.google.com/search?" + urlencode(query)
+    data = requests.get(google, headers=headers)
+    data.encoding = 'ISO-8859-1'
+    soup = BeautifulSoup(str(data.content), "html.parser")
+    try:
+        check = soup.find(id="rso").find("div").find("div").find("h3").find("a")["href"]
+        indexed.append(line)
+    except AttributeError:
+        notindexed.append(line)
+    time.sleep(float(seconds))
+
+
+yind = "\n".join(indexed)
+ny = len(indexed)
+nind = "\n".join(notindexed)
+nn = len(notindexed)
+content1 = "URL of sitemap:\n%s"%(sitemap_url)
+content2 = "There are %d indexed URLs:\n%s"%(ny,yind)
+content3 = "There are %d NOT indexed URLs\n%s"%(nn,nind)
+content  = "\n%s\n\n\n%s\n\n%s"%(content1,content2,content3)
+print(content)
