8459d5a0f336258d958a3072ee969a17e3910ec7
Manu9948 <manoharmadanu@gmail.com>
2018-02-16 10:34:27 +0530

    manu6

--- test3.py
+++ test3.py
@@ -1,12 +1,15 @@
 import urllib2
 
-url = 'http://google.com' # write the url here
+url = 'https://d20kve05iondkm.cloudfront.net/assets/light-vendor-7bb162d30fe5a345edfc448eba5aae1d.js' # write the url here
 
 usock = urllib2.urlopen(url)
 data = usock.read()
 usock.close()
 
+
+
 f=open('data.html','w')
 f.write(data)
 print len(data)
 f.close()
+
