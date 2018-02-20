a533088fe0a6b0d6cbfca82a0e52064421688e00
Manu9948 <manoharmadanu@gmail.com>
2018-02-20 15:34:34 +0530

    manu4

--- test3.py
+++ test3.py
@@ -1,10 +1,6 @@
 import urllib2
 
-<<<<<<< HEAD
-url = 'https://d20kve05iondkm.cloudfront.net/assets/light-vendor-7bb162d30fe5a345edfc448eba5aae1d.js' # write the url here
-=======
-url = 'http://emiratesnbd.deralreserve.com' # write the url here
->>>>>>> 387face1aa5b95064e297f95bdba4a5c0b66036e
+
 
 usock = urllib2.urlopen(url)
 data = usock.read()
